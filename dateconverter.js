892c9044c7ead5bc0698ff75992da6b4659575ae
izaki1994 <34022093+izaki1994@users.noreply.github.com>
2025-09-25 21:58:50 +0900

    copy btn functiona;ity fixed

--- dateconverter.js
+++ dateconverter.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function() {
     //copy Btn
     const copyBtn = document.getElementById('copy-button');
     copyBtn.addEventListener('click', function(){
-        const text = document.getElementById('date-value1').textContent
+        const text = document.getElementById('date-value').textContent
         if(text && text !== 'not set'){
             navigator.clipboard.writeText(text)
             .then(()=>{
